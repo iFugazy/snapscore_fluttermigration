3ad1eff29c179ba54d3fe28fb8179a26d4a51287
fugazy <ackles.darren18@gmail.com>
2024-11-11 19:48:34 -0500

    Added the settings screen and created the functionality to navigate to it from the intro screen.

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:snapscore_fluttermigration/screens/settings_screen.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
@@ -6,13 +7,33 @@ class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Welcome noobs"), ),
       body: Stack(
         children: [
           Positioned.fill(child: Opacity(opacity: 0.5, child: Image.asset("assets/IntroImage.jpg", fit: BoxFit.cover,))),
-          const Center(
-          child: Text("Welcome to the world of Flutter!", style: TextStyle(fontSize: 20)),
-        )],
+          const Align(
+            alignment: Alignment(0, -0.5),
+            child: Text("Welcome to SnapScore", style: TextStyle(
+              fontSize: 20, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.white,
+              shadows: [Shadow(
+                color: Colors.black,
+                blurRadius: 10,
+                offset: Offset(5, 5)
+              )]),),
+          ),
+          Align(
+            alignment: const Alignment(0, 0.5),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const SettingsScreen())
+                );
+              },
+              child: const Text("Get Started"),
+              
+                    ),
+          )],
       ),
     );
   }
